06433ab45939feb1cfdddf4891f7e25be178a796
Vinecosper <vinicosper12@gmail.com>
2025-06-23 18:07:12 -0300

    commit com alguuns ajustes

--- WebAPP/src/app/app.module.ts
+++ WebAPP/src/app/app.module.ts
@@ -1,20 +1,23 @@
+// WebAPP/src/app/app.module.ts
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'; // Importa HTTP_INTERCEPTORS
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 // --- Módulos do PrimeNG ---
 import { ToastModule } from 'primeng/toast';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { ButtonModule } from 'primeng/button';
+import { MessageService } from 'primeng/api'; // 1. IMPORTE O SERVIÇO AQUI
 
 // --- Módulos e Componentes do seu Projeto ---
 import { routes } from './app.routes';
 import { AppComponent } from './app.component';
 import { AppLayoutModule } from './layout/app.layout.module';
-import { AuthInterceptor } from './commons/interceptors/auth.interceptor'; // Importa o novo interceptor
+import { AuthInterceptor } from './commons/interceptors/auth.interceptor';
 
 // --- Componentes Standalone ---
 import { DoacoesCadastroComponent } from './pages/doacoes-cadastro/doacoes-cadastro.component';
@@ -22,36 +25,33 @@ import { DonationSuccessComponent } from './pages/donation-success/donation-succ
 import { DonationCancelComponent } from './pages/donation-cancel/donation-cancel.component';
 import { NotfoundComponent } from './demo/components/notfound/notfound.component';
 import { LoginComponent } from './pages/auth/login/login.component';
-import { CadastroComponent } from './pages/auth/cadastro/cadastro.component'; // Importe o CadastroComponent
+import { CadastroComponent } from './pages/auth/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DoacoesCadastroComponent,
     NotfoundComponent
-    // O array de declarações fica mais vazio, pois a maioria dos componentes é standalone.
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
-
     HttpClientModule,
     AppLayoutModule,
     RouterModule.forRoot(routes),
     ToastModule,
     InputNumberModule,
     ButtonModule,
-
-    // Componentes Standalone são importados aqui, como se fossem módulos
     DonationSuccessComponent,
     DonationCancelComponent,
     LoginComponent,
     CadastroComponent
   ],
   providers: [
-    // Registra o AuthInterceptor para ser usado em todas as requisições HTTP
+    // 2. ADICIONE O MessageService AQUI
+    MessageService,
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
